3daacb9ee5425b94a9078f3d642f020d46aa399b
JATIN <s.jatin1903@gmail.com>
2020-01-12 02:10:18 -0800

    style changes, letting user select, click and flip the camera

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+
+import * as ImagePicker from 'expo-image-picker';
 
 export default class LinkScreen extends React.Component {
 
@@ -11,12 +14,26 @@ export default class LinkScreen extends React.Component {
       setHasPermission: null,
       type: Camera.Constants.Type.back,
       setType: Camera.Constants.Type.back,
-      focusedScreen: false
+      focusedScreen: false,
+      // camera: null,
+      onPictureSaved: null,
+      takeImageText: null,
+      photo: null,
+      status: null,
     }
+
+    this.setState = this.setState.bind(this);
+    this.takePicture = this.takePicture.bind(this);
   }
 
-  componentDidMount(){
+  async componentDidMount(){
     const { navigation } = this.props;
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    
+    this.setState({
+      status: 'granted'
+    })
+
     navigation.addListener('willFocus', () =>
       this.setState({ focusedScreen: true })
     );
@@ -25,28 +42,76 @@ export default class LinkScreen extends React.Component {
     );
   }
 
-  render(){
+  async componentWillUnmount(){
+  }
 
-    const {type, setType, hasPermission, setHasPermission} = this.state;
-
-  // useEffect(() => {
+  // useEffect = (() => {
   //   (async () => {
-  //     const { status } = await Camera.requestPermissionsAsync();
-  //     setHasPermission(status === 'granted');
-  //   })();
+      
   // }, []);
 
-  // if (hasPermission === null) {
+
+//   takePicture = () => {
+    // 
+//     this.setState({
+//         takeImageText: "... PROCESSING PICTURE ..."
+//     }); 
+//     Camera.takePictureAsync({ skipProcessing: true }).then((data) => {
+//         this.setState({
+//             takeImageText: "PICTURE TAKEN",
+//             photo: data.uri
+//         }, console.log(data.uri))
+//     })
+// }
+// 
+  // takePicture = async() => {
+  //   console.log('yes', this.camera);
+  //   if (this.camera) {
+  //     const data = await this.camera.takePictureAsync()
+  //     .then(data => {
+  //       console.log('data uri:' + data.uri);
+  //       saveImage(data.uri);
+  //     });
+  //     }
+  // };
+
+  async takePicture() {
+    if (this.camera) {
+      const data = await this.camera.takePictureAsync()
+      .then(data => {
+        console.log('data uri:' + data.uri);
+      });
+      }
+  };
+
+  pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    }).then(data => {
+      console.log('data uri:' + data.uri);
+    });
+  }
+
+
+  render(){
+
+    const {type, status, setType, hasPermission, setHasPermission} = this.state;
+
+  // if (status === null) {
   //   return <View />;
   // }
-  // if (hasPermission === false) {
-  //   return <Text>No access to camera</Text>;
-  // }
+  if (status === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     // <View style={{ flex: 1 }}>
       this.state.focusedScreen ? 
       (
-        <Camera style={{ flex: 1 }} type={this.state.type}>
+        <Camera style={{ flex: 1 }} type={this.state.type}
+        ref={ref => {
+          this.camera = ref;
+        }}
+        >
         <View
           style={{
             flex: 1,
@@ -55,20 +120,47 @@ export default class LinkScreen extends React.Component {
           }}>
           <TouchableOpacity
             style={{
-              flex: 0.1,
+              flex: 1,
               alignSelf: 'flex-end',
               alignItems: 'center',
+              backgroundColor: 'transparent'
             }}
-            // onPress={() => {
-            //   setType(
-            //     type === Camera.Constants.Type.back
-            //       ? Camera.Constants.Type.front
-            //       : Camera.Constants.Type.back
-            //   );
-            // }}
+            onPress={() => {
+              this.setState({
+                type: type ? Camera.Constants.Type.back : Camera.Constants.Type.front
+              })
+            }}
             >
             <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
           </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignSelf: 'flex-end',
+              alignItems: 'center',
+              backgroundColor: 'transparent'
+            }}
+            // onPress={() => this.takePicture()}
+            onPress={this.takePicture}
+            >
+            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Click </Text>
+          </TouchableOpacity>
+
+
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignSelf: 'flex-end',
+              alignItems: 'center',
+              backgroundColor: 'transparent'
+            }}
+            // onPress={() => this.takePicture()}
+            onPress={this.pickImage}
+            >
+            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Pick image </Text>
+          </TouchableOpacity>
+
         </View>
       </Camera>
       )
